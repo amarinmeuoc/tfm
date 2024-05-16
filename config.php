b2d5482929636b57ed6c79fe20a1ac813d56c0c7
Alberto Marín Mendoza <amarinme@uoc.edu>
2024-05-16 17:40:06 +0200

    config.php

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'tfmdatabase';
+$CFG->dbuser    = 'tfmuser';
+$CFG->dbpass    = '2m3ixbsx';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_0900_ai_ci',
+);
+
+$CFG->wwwroot   = 'http://tfm.com';
+$CFG->dataroot  = '/home/alberto/public_html/tfmdata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
